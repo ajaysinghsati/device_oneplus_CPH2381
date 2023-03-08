1cf5668ae12e7acc8827a0495f60c33c856eda01
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-03-08 15:48:31 +0530

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -8,8 +8,8 @@
 
 set -e
 
-DEVICE=holi
-VENDOR=qualcomm
+DEVICE=**** FILL IN DEVICE NAME ****
+VENDOR=**** FILL IN VENDOR NAME ****
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -32,10 +32,6 @@ SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-caf )
-                ONLY_CAF=true
-                CLEAN_VENDOR=false
-                ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -43,7 +39,7 @@ while [ "${#}" -gt 0 ]; do
                 KANG="--kang"
                 ;;
         -s | --section )
-                SECTION="${3}"; shift
+                SECTION="${2}"; shift
                 CLEAN_VENDOR=false
                 ;;
         * )
@@ -59,46 +55,25 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service)
-            grep -q libshims_fingerprint.oplus.so "${2}" || "${PATCHELF}" --add-needed libshims_fingerprint.oplus.so "${2}"
+        vendor/lib/libsample1.so)
+            sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
             ;;
-        odm/etc/vintf/manifest/manifest_oplus_fingerprint.xml)
-            sed -ni "/android.hardware.biometrics.fingerprint/{x;s/hal format/hal override=\"true\" format/;x};x;1!p;\${x;p}" "${2}"
+        vendor/lib64/libsample2.so)
+            "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
+            "${PATCHELF}" --add-needed "libsample4.so" "${2}"
             ;;
-        product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml)
-            sed -i "s/\/my_product/\/product/" "${2}"
+        vendor/lib/libsample5.so)
+            "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
             ;;
-        system_ext/lib64/libwfdnative.so)
-            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
-            ;;
-        vendor/etc/libnfc-nxp.conf)
-            sed -i "/NXP_NFC_DEV_NODE/ s/pn553/nq-nci/" "${2}"
-            ;;
-        vendor/lib64/hw/com.qti.chi.override.so)
-            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
-            sed -i "s/com.oem.autotest/\x00om.oem.autotest/" "${2}"
-            ;;
-        vendor/lib64/sensors.ssc.so)
-            sed -i "s/qti.sensor.wise_light/android.sensor.light\x00/" "${2}"
-            "${SIGSCAN}" -p "F1 E9 D3 84 52 49 3F A0 72" -P "F1 A9 00 80 52 09 00 A0 72" -f "${2}"
-            ;;
-        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "1F 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+        vendor/lib/libsample7.so)
+            "${PATCHELF}" --set-soname "libsample7.so" "${2}"
             ;;
     esac
 }
 
 # Initialize the helper
-if [ -z "${ONLY_CAF}" ]; then
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ ! -z "${ONLY_CAF}" ]; then
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
-
-    extract "${MY_DIR}/proprietary-files-caf.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
