4195d57f772574c656bce690e57eaa5878cae322
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-03-10 16:25:50 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,8 +2,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_CPH2381.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_avicii-userdebug \
-    lineage_avicii-eng
+    lineage_CPH2381-userdebug \
+    lineage_CPH2381-eng
 
     
     
