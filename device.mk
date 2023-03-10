bdd92af5b3567e971ab5e084131f9c84b38048ef
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-03-10 17:15:09 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -152,9 +152,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
 # Boot control
 PRODUCT_PACKAGES += \
